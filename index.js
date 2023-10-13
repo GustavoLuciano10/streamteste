d66d18721637a182069d2bf7b56656b5e78735b9
GustavoLuciano10 <49996125+GustavoLuciano10@users.noreply.github.com>
2023-10-13 19:04:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
-const NodeMediaServer = require('node-media-server');
 const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
 const ffmpeg = require('fluent-ffmpeg');
 ffmpeg.setFfmpegPath(ffmpegPath);
 
-const config = {
-  rtmp: {
-    port: 1935,
-    chunk_size: 60000,
-    gop_cache: true,
-    ping: 30,
-    ping_timeout: 60
-  },
-  http: {
-    port: 8000,
-    allow_origin: '*'
-  }
-};
+var proc3;
+let streamYT = (YTrtmp) => {
+    console.log("streaming to youtube")
+      proc3 = new ffmpeg({ source: 'http://acsa.ws:80/r4tSBeEJ/frogtvteste/72040.m3u8', timeout: 0 })
+      .addOption('-vcodec', 'libx264')
+      .addOption('-acodec', 'aac')
+      .addOption('-crf', 26)
+      .addOption('-aspect', '1920:1080')
+      .addOption('-f', 'flv')
+      .withSize('1920x1080')
+      .on('start', function(commandLine) {
+      console.log('Query : ' + commandLine);
+      })
+      .on('error', function(err) {
+      console.log('Error: ' + err.message);
+      })
+      .output('rtmp://localhost/live/stream' + YTrtmp, function(stdout, stderr) {
+        console.log('Convert complete' +stdout)
+      })
+      .run()
+    }
+    streamYT('')
 
-var nms = new NodeMediaServer(config)
-nms.run();
+    console.log(proc3)
