596cbd0614053ff101a4b6216355200be2426e1b
GustavoLuciano10 <49996125+GustavoLuciano10@users.noreply.github.com>
2023-10-13 17:43:25 -0300

    Update index.js

--- index.js
+++ index.js
@@ -19,28 +19,3 @@ const config = {
 
 var nms = new NodeMediaServer(config)
 nms.run();
-
-var proc3;
-let streamYT = (YTrtmp) => {
-    console.log("streaming to youtube")
-      proc3 = new ffmpeg({ source: 'http://acsa.ws:80/r4tSBeEJ/frogtvteste/72040.m3u8', timeout: 0 })
-      .addOption('-vcodec', 'libx264')
-      .addOption('-acodec', 'aac')
-      .addOption('-crf', 26)
-      .addOption('-aspect', '1920:1080')
-      .addOption('-f', 'flv')
-      .withSize('1920x1080')
-      .on('start', function(commandLine) {
-      console.log('Query : ' + commandLine);
-      })
-      .on('error', function(err) {
-      console.log('Error: ' + err.message);
-      })
-      .output('rtmp://localhost/live/stream' + YTrtmp, function(stdout, stderr) {
-        console.log('Convert complete' +stdout)
-      })
-      .run()
-    }
-    streamYT('')
-
-    console.log(proc3)
