c7b6391b8d5a013fec32c4ce338cad7a000c4b96
GustavoLuciano10 <49996125+GustavoLuciano10@users.noreply.github.com>
2023-10-13 18:14:50 -0300

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,1 +1,21 @@
+const NodeMediaServer = require('node-media-server');
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+const ffmpeg = require('fluent-ffmpeg');
+ffmpeg.setFfmpegPath(ffmpegPath);
 
+const config = {
+  rtmp: {
+    port: 1935,
+    chunk_size: 60000,
+    gop_cache: true,
+    ping: 30,
+    ping_timeout: 60
+  },
+  http: {
+    port: 8000,
+    allow_origin: '*'
+  }
+};
+
+var nms = new NodeMediaServer(config)
+nms.run();
